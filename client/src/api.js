8709cef0160427db2cc6deefe58096f27af21b5b
ag <cold@192.168.1.7>
2026-01-17 18:01:01 +0530

    backend deployed, updated API_URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
 
 export const getEmployees = () => axios.get(`${API_URL}/employees/`);
 
